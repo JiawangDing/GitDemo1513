7f125ff230ce94f8698031f1bbf9e3c207f2d73c
dingjiawang <dingjiawang2012@163.com>
2015-11-25 22:54:15 +0800

    B commit second

---
+++ GitDemo/GitDemo/ViewControllerB.h
@@ -0,0 +1,13 @@
+//
+//  ViewControllerB.h
+//  GitDemo
+//
+//  Created by qianfeng007 on 15/11/25.
+//  Copyright © 2015年 JiawangDing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewControllerB : UIViewController
+
+@end
